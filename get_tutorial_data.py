0ce8b071267fec00e516ad0dddb62c819b5edbec
taylor-cory <taylor_cory@bah.com>
2017-06-24 11:54:29 -0400

    removed pickles, added sample config.py

--- get_tutorial_data.py
+++ get_tutorial_data.py
@@ -5,5 +5,5 @@ install_dir = os.path.abspath(os.path.dirname(__file__))
 data_dir = os.path.join(install_dir, 'tutorial_data')
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
-#download('twitter_samples', download_dir=data_dir)
+download('twitter_samples', download_dir=data_dir)
 download('all')
---
+++ ~config.py
@@ -0,0 +1,12 @@
+twitter_ckey = ''
+twitter_csecret = ''
+twitter_atkn = ''
+twitter_asecret = ''
+
+search_terms = [
+    "augusta",
+    "python"
+]
+
+classifier_pickle = "dt_classifier.pickle"
+tokens_pickle = "tokens.pickle"
